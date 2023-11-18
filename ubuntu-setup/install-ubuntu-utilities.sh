84e61b96e82e803e22b60e10134ead9e7e1cce73
Andrew Miller <Andrew.patrick.miller.2021@gmail.com>
2023-11-18 22:53:20 +0000

    added nvm setup

--- ubuntu-setup/install-ubuntu-utilities.sh
+++ ubuntu-setup/install-ubuntu-utilities.sh
@@ -1,3 +1,7 @@
 sudo apt-get update
 sudo apt-get install -y gh tree tmux nmap
 git clone --depth 1 https://github.com/Milldrew/simple-vim-config.git
+
+# github copilot for vim
+git clone https://github.com/github/copilot.vim.git \
+  ~/.vim/pack/github/start/copilot.vim
---
+++ ubuntu-setup/update-vim.sh
@@ -0,0 +1,7 @@
+sudo add-apt-repository ppa:jonathonf/vim
+
+sudo apt update
+
+sudo apt-get install vim
+
+vim --version
